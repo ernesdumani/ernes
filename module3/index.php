099d6305fef821e05a3f84610bb37eb9cd7286c0
Studenti <ernesdumani@gmail.com>
2024-10-16 17:48:04 +0200

    Create index.php

---
+++ module3/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$num = -1;
+if($num<0){
+  //   echo "$num is less than 0";
+}
+  
+
+///$age = 16;
+//if(($age> 14) && ($age< 18)){
+    //echo "sale for you!!";
+//}
+
+//($age < 18){
+   // echo "you are an adult";
+
+//}else {
+   // echo "you are underage";
+//}
+//($num<O){
+//echo " the v alue of /$num is a negative number!!!";
+//}elseif{$num==0}{
+// echo " the value of/$num is equal to zero";
+
+//}else{
+    //echo "the value of/$num is a positiove number";
+//}
+// Write a program that takes a student's score as input and outputs their grades with this system: A: 90-100, B: 80-89, C:70-79, D: 60-69, F: below 60
+$score = 100
+if (($score >= 90) && ($score =100 )){
+    echo "you got an A";
+}
+?>
